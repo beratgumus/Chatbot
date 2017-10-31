cc807e4a5b167e18f1b7a68d0eac002fbcdec1c8
Berat Gumus <beratgumus9@gmail.com>
2017-10-31 23:45:59 +0300

    Some arrangement

--- src/TwitterAPI.java
+++ src/TwitterAPI.java
@@ -6,10 +6,16 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+ *   Static class for getting a comments from twitter app.
+ */
 public class TwitterAPI {
-
+    /*
+    * Static method that returns the list of Tweet according to keyword.
+    * @param keyword that used for searching a comments.
+    */
     public static List<Tweet> getTweets(String searchKey) throws TwitterException {
-
+        //Connecting to a twitter app. You have to use your Consumer Keys and Access Tokens !!!!
         ConfigurationBuilder cb = new ConfigurationBuilder();
         cb.setDebugEnabled(true).setOAuthConsumerKey("*****")
                 .setOAuthConsumerSecret("********")
@@ -20,19 +26,19 @@ public class TwitterAPI {
         twitter4j.Twitter twitter = tf.getInstance();
         List<Tweet> tweetList = new ArrayList<Tweet>();
         try {
-            Query query = new Query("(#"+searchKey+") AND ((good) OR (bad)) exclude:retweets exclude:links");
+            //ToDo: need to improvment on query
+            Query query = new Query("(#" + searchKey + ") AND ((good) OR (bad)) exclude:retweets exclude:links");
             query.count(10);
             QueryResult result = twitter.search(query);
             List<Status> tweets = result.getTweets();
-
-            double reviewPoint=0.00;
+            double reviewPoint;
             DateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
-            query.count(10);
 
             for (Status tweet : tweets) {
-                System.out.println("@" + tweet.getUser().getScreenName() + " [id]: "+tweet.getId() + " : " + tweet.getText()+"[date]: "+tweet.getCreatedAt()+"\n");
-                reviewPoint=(double) (Math.random()*100);
-                Tweet newTweet = new Tweet(tweet.getId(),tweet.getText(),tweet.getUser().getScreenName(),df.format(tweet.getCreatedAt()),reviewPoint);
+                System.out.println("@" + tweet.getUser().getScreenName() + " [id]: " + tweet.getId() + " : " + tweet.getText() + "[date]: " + tweet.getCreatedAt() + "\n");
+                // ToDo: It is just for test, need to improve reviewPoint calculation
+                reviewPoint = Math.random() * 100;
+                Tweet newTweet = new Tweet(tweet.getId(), tweet.getText(), tweet.getUser().getScreenName(), df.format(tweet.getCreatedAt()), reviewPoint);
                 tweetList.add(newTweet);
             }
         } catch (TwitterException te) {
@@ -40,7 +46,7 @@ public class TwitterAPI {
             System.out.println("Failed to search tweets: " + te.getMessage());
         }
 
-       return tweetList;
+        return tweetList;
     }
 
 }
