6dfce8f7fa9b3914663183a5f6575ac885a4ab44
Bekir Uzun <bekiruzun8000@gmail.com>
2017-11-03 23:31:20 +0300

    Improved SenticNet

--- src/SenticNet.java
+++ src/SenticNet.java
@@ -1,51 +1,52 @@
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
-
 
 public class SenticNet {
     private Scanner scanner;
+    private HashMap<String, Double> data;
 
     public SenticNet() {
 
-        String file = "..\\Chatbot\\src\\test.txt";
+        String file = "..\\Chatbot\\src\\senticnet4-v2.txt";
         try {
             scanner = new Scanner(new File(file));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+        data = new HashMap<>();
+
+        //put values from text to hashmap
+        while (scanner.hasNextLine()) {
+            String currentLine = scanner.nextLine();
+            if (!currentLine.contains("_")) {
+                String parts[] = currentLine.split(" ");
+                double val = Double.parseDouble(parts[1]);
+                data.put(parts[0], val);
+            }
+        }
 
     }
 
+    /**
+     * Calculates review point of given string from SenticNet
+     * @param tweetText text
+     * @return calculated point
+     */
     public double calculateReviewPoint(String tweetText) {
         Double sum = 0.0;
-        String sentence = tweetText;
-        ArrayList<String> vocab = new ArrayList<>();
-        String[] sp = sentence.split(" ");
-        for (int i = 0; i < sp.length; i++) {
-            vocab.add(sp[i]);
+        tweetText = tweetText.replaceAll("[^a-zA-Z ]+","");
+        String[] words = tweetText.split(" ");
+        for (String word : words) {
+            Double polarity = data.get(word);
+            if (polarity != null) {
+                sum += polarity;
+                System.out.println(word + " : " + polarity);
+            }
         }
-        for (int i = 0; i < vocab.size(); i++) {
-            Double polarity = getPolarity(vocab.get(i));
-            sum += polarity;
-            System.out.println(vocab.get(i) + " : " + polarity);
-        }
-
         return sum;
     }
 
-    public double getPolarity(String s) {
-        double reviewValue = 0.00;
-        boolean found = false;
-        while (scanner.hasNextLine() && !found) {
-            String currentLine = scanner.nextLine();
-            if (currentLine.contains(s)) {
-                reviewValue += Double.parseDouble(currentLine.split(" ")[1]);
-                found = true;
-            }
-        }
-        return reviewValue;
-    }
 }
--- src/SenticNetTest.java
+++ src/SenticNetTest.java
@@ -2,9 +2,8 @@
 public class SenticNetTest {
     public static void main(String args[]) {
 
-
         SenticNet sentic = new SenticNet();
-        double result = sentic.calculateReviewPoint("berat gumus bekir");
+        double result = sentic.calculateReviewPoint("my brand new iphonex rocks! its really awesome!");
         System.out.println(result);
     }
 
