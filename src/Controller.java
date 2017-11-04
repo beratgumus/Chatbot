bda8c6efbd6f07322866175c567a6d3f5a630fe2
Bekir Uzun <bekiruzun8000@gmail.com>
2017-11-05 01:18:52 +0300

    Added form validation

--- src/Controller.java
+++ src/Controller.java
@@ -5,6 +5,7 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import twitter4j.TwitterException;
@@ -29,7 +30,15 @@ public class Controller {
     public TextField screenTF;
     public TextField cameraTF;
     public TextField ramTF;
+
+    @FXML
+    public Label messageLabel;
+    public Rectangle messageBox;
+
     TwitterAPI twitterAPI;
+    private static final String infoColor = "#3c9ae1";
+    private static final String errorColor = "#dd3b3b";
+    private static final String successColor = "#3ce07e";
 
     /**
      * This function is called by close button at the top right
@@ -45,36 +54,58 @@ public class Controller {
      */
     @FXML
     public void savePhone(Event event) {
-        MobilePhone newMobilePhone = new MobilePhone(
-                brandTF.getText(),
-                modelTF.getText(),
-                Double.parseDouble(priceTF.getText()),
-                Double.parseDouble(heightTF.getText()),
-                Double.parseDouble(widthTF.getText()),
-                Double.parseDouble(depthTF.getText()),
-                Integer.parseInt(weightTF.getText()),
-                Double.parseDouble(screenTF.getText()),
-                Integer.parseInt(storageTF.getText()),
-                Integer.parseInt(cameraTF.getText()),
-                osTF.getText(),
-                Integer.parseInt(ramTF.getText()));
+
+        if (!isSet(brandTF) || !isSet(modelTF) || !isSet(priceTF) || !isSet(osTF)
+                || !isSet(heightTF) || !isSet(widthTF) || !isSet(depthTF) || !isSet(weightTF)
+                || !isSet(screenTF) || !isSet(storageTF) || !isSet(cameraTF) || !isSet(ramTF)){
+            messageLabel.setText("You must fill the form");
+            messageBox.setFill(Color.web(errorColor));
+            return;
+        } else {
+            messageBox.setFill(Color.web(infoColor));
+        }
+
+        MobilePhone newMobilePhone;
+        try {
+            newMobilePhone = new MobilePhone(
+                    brandTF.getText(),
+                    modelTF.getText(),
+                    Double.parseDouble(priceTF.getText()),
+                    Double.parseDouble(heightTF.getText()),
+                    Double.parseDouble(widthTF.getText()),
+                    Double.parseDouble(depthTF.getText()),
+                    Integer.parseInt(weightTF.getText()),
+                    Double.parseDouble(screenTF.getText()),
+                    Integer.parseInt(storageTF.getText()),
+                    Integer.parseInt(cameraTF.getText()),
+                    osTF.getText(),
+                    Integer.parseInt(ramTF.getText()));
+        } catch (Exception e) {
+            messageLabel.setText("One of values is not acceptable");
+            messageBox.setFill(Color.web(errorColor));
+            return;
+        }
+
         try {
             twitterAPI = new TwitterAPI();
             double reviewPoint = twitterAPI.getReviewPoint(modelTF.getText());
             System.out.println("10 tweetin Hesaplanan ortalamasi: " + reviewPoint);
             newMobilePhone.setReviewPoint(reviewPoint);
         } catch (TwitterException e) {
-
+            newMobilePhone.setReviewPoint(0.0);
         }
-//        newMobilePhone.setReviewPoint(2.2);
-//        /**
-//         * ToDo: insert the object to mongoDB
-//         */
-//        ProductDB mongoDB = new ProductDB();
-//        mongoDB.insert(newMobilePhone);
-//        mongoDB.close(); //close db connection
+
+        ProductDB mongoDB = new ProductDB();
+        mongoDB.insert(newMobilePhone);
+        mongoDB.close(); //close db connection
 
         this.close();
     }
 
+    private boolean isSet(TextField tf) {
+        if (tf.getText().isEmpty() || tf.getText().equals("") || !tf.getText().matches(".*[a-zA-Z0-9]+.*"))
+            return false;
+        else
+            return true;
+    }
 }
