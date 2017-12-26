41bbd4381f5a0e075582285e9a9fb4587693a7c0
Berat Gumus <beratgumus9@gmail.com>
2017-12-26 11:37:15 +0300

    Fix on tests

--- tests/RedisTest.java
+++ tests/RedisTest.java
@@ -2,6 +2,8 @@ import apis.Redis;
 import apis.SenticNet;
 import apis.Tweet;
 import org.junit.Test;
+import strategyPattern.Calculation;
+import strategyPattern.CalculationStrategy1;
 
 import java.util.List;
 
@@ -9,16 +11,22 @@ import static org.junit.Assert.*;
 
 
 public class RedisTest {
-    Redis redis = Redis.getInstance();
-    SenticNet sn = SenticNet.getInstance();
+    private Redis redis = Redis.getInstance();
 
     @Test // check the review point calculation of all tweets for specified product.
     public void getTweetsByKeyword() throws Exception {
         String keyword ="iPhoneX";
         List<Tweet> tweetList = redis.getTweetsByKeyword(keyword);
+
         for (Tweet tweet : tweetList){
-           double storedReviewPoint = tweet.getReviewPoint();
-           double actual = sn.calculateReviewPoint(tweet.getText());
+           double storedReviewPoint = tweet.getcalStrategy1();
+
+            Calculation calculationStrategy1 = new CalculationStrategy1();
+            double actual = calculationStrategy1.calculate(tweet.getText());
+
+            Calculation calculationStrategy2 = new CalculationStrategy1();
+            double actual2 = calculationStrategy2.calculate(tweet.getText());
+
            assertEquals(storedReviewPoint,actual,0.000);
         }
     }
